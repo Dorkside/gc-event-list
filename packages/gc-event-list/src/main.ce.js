5c2097a8d31da4a045f1d5155e7ccb09d97e8d79
Dorkside <jdmartin@gmx.fr>
2023-03-11 15:53:14 +0100

    fix: more vonfig for build

--- packages/gc-event-list/src/main.ce.js
+++ packages/gc-event-list/src/main.ce.js
@@ -6,9 +6,5 @@ import EventItem from './EventItem.ce.vue'
 const EventListElement = defineCustomElement(EventList)
 const EventItemElement = defineCustomElement(EventItem)
 
-export { EventListElement, EventItemElement }
-
-export function register() {
-  customElements.define('event-list', EventListElement)
-  customElements.define('event-item', EventItemElement)  
-}
+customElements.define('event-list', EventListElement)
+customElements.define('event-item', EventItemElement)  
